23fb8a591fd846d4bc679476d9bf3fb814c8f973
zyg922 <1405679773@qq.com>
2021-01-23 23:13:51 +0800

    first commit

---
+++ src/main/java/com/sast/atSast/AtSastApplication.java
@@ -0,0 +1,13 @@
+package com.sast.atSast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtSastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtSastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sast/atSast/AtSastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sast.atSast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtSastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
