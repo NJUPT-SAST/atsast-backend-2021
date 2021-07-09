f6fd2219adea5441fc2ba0476f14cab39d3edb0b
Xander-C <1019104544@qq.com>
2021-07-09 18:11:25 +0800

    fix: duplicate all args constructor

--- src/main/java/com/sast/atSast/model/Contest.java
+++ src/main/java/com/sast/atSast/model/Contest.java
@@ -30,26 +30,4 @@ public class Contest {
   private String comment;
   private byte curr;
   private Byte enable;
-
-  public Contest(long masterUid, long contestId, String name, String description, Date reg, Date submit, Date judge, Date end, Date submitEnd, Date judgeEnd, Date endEnd, boolean isTeam, String instructor, String videoId, String picId, boolean isJoin, String comment, byte curr, Byte enable) {
-    this.masterUid = masterUid;
-    this.contestId = contestId;
-    this.name = name;
-    this.description = description;
-    this.reg = reg;
-    this.submit = submit;
-    this.judge = judge;
-    this.end = end;
-    this.submitEnd = submitEnd;
-    this.judgeEnd = judgeEnd;
-    this.endEnd = endEnd;
-    this.isTeam = isTeam;
-    this.instructor = instructor;
-    this.videoId = videoId;
-    this.picId = picId;
-    this.isJoin = isJoin;
-    this.comment = comment;
-    this.curr = curr;
-    this.enable = enable;
-  }
 }
